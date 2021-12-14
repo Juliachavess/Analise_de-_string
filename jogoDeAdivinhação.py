a9c5cd496f515131097f5741374984b4524be1eb
julia <juliachavess@gmail.com>
2021-12-14 18:01:07 -0300

    processando

--- jogoDeAdivinhação.py
+++ jogoDeAdivinhação.py
@@ -1,7 +1,10 @@
 import random
+from time import sleep
 numero = random.randint(0,5)
 print('Adivinhe um número de 0 a 5.')
 usuario = int(input('Digite seu número:'))
+print('PROCESSANDO...')
+sleep(2)
 if usuario == numero:
     print('Você venceu')
 else:
