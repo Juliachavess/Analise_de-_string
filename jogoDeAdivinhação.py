8078a3d69a74c9cb47321677030a68795a93bbc7
julia <juliachavess@gmail.com>
2021-12-14 17:54:15 -0300

    mensagem para o usuário

--- jogoDeAdivinhação.py
+++ jogoDeAdivinhação.py
@@ -1,9 +1,9 @@
 import random
 numero = random.randint(0,5)
 print('Adivinhe um número de 0 a 5.')
-usuario = int(input('Adivinhe o número:'))
+usuario = int(input('Digite seu número:'))
 if usuario == numero:
     print('Você venceu')
 else:
-    print('Você perdeu!')
+    print('Você perdeu! Eu pensei no número {} e não no número {}.'.format(numero,usuario))
 print('FIM')
